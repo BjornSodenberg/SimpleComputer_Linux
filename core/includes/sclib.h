106123a245d62336dd7318b7f9431d866c7c7276
BjornSodenberg <garonini1@gmail.com>
2017-02-18 20:25:19 +0700

    Реализована сборка библиотеки sclib.a

--- core/includes/sclib.h
+++ core/includes/sclib.h
@@ -7,6 +7,23 @@
 #include <limits.h>
 
 #define MEMSIZE 100
+#define FLAG_OVERFLOW 0
+#define FLAG_DIVISION 1
+#define FLAG_OUTMEM 2
+#define FLAG_INTERRUPT 3
+#define FLAG_COMMAND 4
+#define FLAG_ODD 5
+
+#define ERR_WRONG_ADDR -1
+#define ERR_OPEN_FILE -2
+#define ERR_FILE_SIZE -3
+#define ERR_UNCORRECT_COMMAND -4
+#define ERR_ATTRIBUTE_BIT -5
+#define ERR_WRONG_FLAG -6
+
+#define BIT_SET(A, B) (A =((A) | (1 << (B))))
+#define BIT_CLEAR(A, B) (A = ((A) & ~(1 << (B))))
+#define BIT_CHECK(A, B) (((A) >> B) & 1)
 
 extern int sc_memory[MEMSIZE];
 
